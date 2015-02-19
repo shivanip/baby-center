1e9238aac9a44dc6cc52b087f804858bdd072b1c
Shivi <spandey2122@gmail.com>
2015-02-19 18:03:14 -0500

    Created index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+    resources :books, :only => [:index]
+    root 'books#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
